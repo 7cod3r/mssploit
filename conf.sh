c158f3ac4cd2dfb60b9b9c957d14477531bcd07f
Rooj Official <72885447+the-rooj@users.noreply.github.com>
2021-10-07 20:37:55 +0300

    Update conf.sh

--- conf.sh
+++ conf.sh
@@ -6,13 +6,15 @@ termux-setup-storage > /dev/null 2>&1 &
 
 banner(){ 
     echo -e "
-                                        
-     \e[32m_____     ___\e[0m    \e[31m_____       _         \e[0m
-    \e[32m|  |  |___| | |\e[0m  \e[31m|     |___ _| |___ ___ \e[0m
-    \e[32m|     | -_|_  |\e[0m  \e[31m|   --| . | . | -_|  _|\e[0m
-    \e[32m|__|__|___| |_|\e[0m  \e[31m|_____|___|___|___|_|  \e[0m
-  
-    
+   
+    \e[32m██████╗  ██████╗  ██████╗      ██╗\e[0m    \e[31m ██████╗ ███████╗███████╗██╗ ██████╗██╗ █████╗ ██╗     \e[0m
+    \e[32m██╔══██╗██╔═══██╗██╔═══██╗     ██║\e[0m    \e[31m██╔═══██╗██╔════╝██╔════╝██║██╔════╝██║██╔══██╗██║     \e[0m
+    \e[32m██████╔╝██║   ██║██║   ██║     ██║\e[0m    \e[31m██║   ██║█████╗  █████╗  ██║██║     ██║███████║██║     \e[0m
+    \e[32m██╔══██╗██║   ██║██║   ██║██   ██║\e[0m    \e[31m██║   ██║██╔══╝  ██╔══╝  ██║██║     ██║██╔══██║██║     \e[0m
+    \e[32m██║  ██║╚██████╔╝╚██████╔╝╚█████╔╝\e[0m    \e[31m╚██████╔╝██║     ██║     ██║╚██████╗██║██║  ██║███████╗\e[0m
+    \e[32m╚═╝  ╚═╝ ╚═════╝  ╚═════╝  ╚════╝\e[0m     \e[31m ╚═════╝ ╚═╝     ╚═╝     ╚═╝ ╚═════╝╚═╝╚═╝  ╚═╝╚══════╝\e[0m
+                                                                                             
+
 "
 echo -e "  \e[32mBU PROGRAM SADECE TERMUXTA ÇALIŞMAKTADIR..."
 
@@ -28,7 +30,7 @@ banner
 			    read -p $'                   >\033[31m ' rooj
     if [[ $rooj == y ||  $rooj == Y  ]]; then
         sleep 2
-        chmod 777 conf.sh
+        chmod 777 conf
         chmod 777 mssploit
         cp -r mssploit /data/data/com.termux/files/usr/bin && cd $HOME && mssploit
 
@@ -44,7 +46,7 @@ banner
         echo ""
         sleep 2
         pkg install git curl wget nmap -y && wget https://github.com/Hax4us/TermuxBlack/raw/master/install.sh && chmod +x install.sh && ./install.sh -i && pkg install ruby2 && wget https://raw.githubusercontent.com/Hax4us/Metasploit_termux/master/metasploit.sh && chmod +x metasploit.sh && ./metasploit.sh
-        chmod 777 conf.sh
+        chmod 777 conf
         chmod 777 mssploit
         cp -r mssploit /data/data/com.termux/files/usr/bin && cd $HOME && mssploit
 
@@ -52,5 +54,5 @@ banner
         echo -e " \e[31m Hatalı seçim! "
         sleep 1
         clear
-        bash conf.sh
+        ./conf
     fi
